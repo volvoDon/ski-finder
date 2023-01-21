4ef8c460d957946f0eb459170d32c5c3e6a99742
Sam Segal <samsegal@sams-mbp.lan>
2023-01-21 14:31:53 -0700

    working with two sites

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template
 import lib
-from lib import backcountry 
+from lib import backcountry,evo 
 
 app = Flask(__name__)
 
@@ -12,6 +12,8 @@ def landing():
 @app.route("/skis")
 def skis():
     skis = backcountry.scrape_backcountry()
-    return render_template("skis.html",skis=skis)
+    products_from_evo = evo.scrape_evo()
+    fulllist = skis + products_from_evo
+    return render_template("skis.html",skis=fulllist)
 
 
--- lib/evo.py
+++ lib/evo.py
@@ -1,27 +1,41 @@
 import requests as req;
 from bs4 import BeautifulSoup;
 URL = "https://www.evo.com/shop/sale/ski/skis/ability_advanced-expert"
-def scrape_backcountry():
+def scrape_evo():
     product_from_evo = [] #Brand,Title,img_src,price,compare_at,link
     entire_page = req.get(URL)
     soup = BeautifulSoup(entire_page.content,"html.parser")
     all_products = soup.find_all("div",{"class":"product-thumb-details"})
     for i,product in enumerate(all_products):
         temp = []
-        if i == 0 :
+        #if i == 0 :
+            #print(product.prettify())
+
+        if i < 20 :
             titleString = product.find("span",{"class":"product-thumb-title"}).getText().split()
             brand = titleString [0]
             title = ' '.join(titleString[1:])
             img = product.find("img")["src"]
             price = product.find_all("span",{"class":"product-thumb-price"})[1].getText().split()[1]
             compare_at = product.find("span",{"class":"product-thumb-price"}).getText() 
-            print(product.prettify())
+            link = product.find("a")["href"]
+            link = "https://www.evo.com"+link
+        
             print("---------------") 
             print(brand)
             print(title)
             print(img)
             print(price)
             print(compare_at)
+            print(link)
+            temp.append(brand)
+            temp.append(title)
+            temp.append(img)
+            temp.append(price)
+            temp.append(compare_at)
+            temp.append(link)
+            product_from_evo.append(temp)
+    return product_from_evo           
 
 
-scrape_backcountry()
+scrape_evo()
