11f3477d5d7ed929b3aa1681f86abf438a8bce18
Sam Segal <samsegal@sams-mbp.lan>
2023-01-22 20:29:31 -0700

    broken install the sdk

--- lib/database_portal.py
+++ lib/database_portal.py
@@ -1,17 +1,34 @@
-import mysql.connector
+from google.cloud.sql.connector import Connector
+import pymysql
+import os
+import sqlalchemy
 TESTARRAY = [1,2,3,4,5,6,7]
 TESTARRAY_SHORT = [1,2,3,4,5,6]
-mydb = mysql.connector.connect(
-  host='localhost',
-  user="root",
-  database="portal",
+# function to return the database connection
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+connector = Connector(
+    enable_iam_auth=False,
+    timeout=30,
+)
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        "skifinder:us-central1:ski-finder-data",
+        "pymysql",
+        password=os.getenv("PASSWORD"),
+        db="portal"
+    )
+    return conn
+
+pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
 )
 
-mycursor = mydb.cursor()
 
 def clear_db ():
-    mycursor.execute("DELETE FROM product")
-    mydb.commit()
+    stmt = sqlalchemy.text("DELETE FROM product")
+    with pool.connect() as db_con:
+      db_con.execute(stmt)
 
 def add_row (row):
     if len(row) == 7:
@@ -22,15 +39,17 @@ def add_row (row):
     
     val = tuple(row)  
      
-    mycursor.execute(sql,val)
-    mydb.commit()
+    with pool.connect() as db_con:
+      db_con.execute(sql,val)
 
 def get_all():
     final = []
-    mycursor.execute("SELECT * FROM product")
-    results = mycursor.fetchall()
-    for item in results:
-      final.append(list(item))
+    with pool.connect() as db_con:
+      results = db_con.execute("SELECT * FROM product").fetchall()
+      for item in results:
+        final.append(list(item))
     return final
+
+clear_db()    
 
 
