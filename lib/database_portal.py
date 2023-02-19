63c0e0b55a832a00a25ca8e2b2460f1d622f8e61
Sam Segal <samsegal@sams-mbp.lan>
2023-02-19 15:44:27 -0700

    not done

--- lib/database_portal.py
+++ lib/database_portal.py
@@ -1,22 +1,16 @@
-from google.cloud.sql.connector import Connector
-import pymysql
+import database
 import os
-import sqlalchemy
-TESTARRAY = [1,2,3,4,5,6,7]
-TESTARRAY_SHORT = [1,2,3,4,5,6]
-# function to return the database connection
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-connector = Connector(
-    enable_iam_auth=False,
-    timeout=30,
-)
-def getconn() -> pymysql.connections.Connection:
-    conn: pymysql.connections.Connection = connector.connect(
-        "skifinder:us-central1:ski-finder-data",
-        "pymysql",
-        password=os.getenv("PASSWORD"),
-        db="portal"
-    )
+from sqlalchemy import text
+
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            "skifinder:us-central1:ski-finder-data", # Cloud SQL Instance Connection Name
+            "mysql",
+            user="root",
+            password=os.getenv("PASSWORD"),
+            db="portal",
+        )
     return conn
 
 pool = sqlalchemy.create_engine(
@@ -50,6 +44,4 @@ def get_all():
         final.append(list(item))
     return final
 
-clear_db()    
-
-
+print(get_all())
